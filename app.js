354a6048898e055a753ac7eced3f8681b14b3f1c
Shiva Gurung <shivakgrg@gmail.com>
2023-09-28 12:45:44 +0545

    DB, modal and store register user data in DB

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const { users } = require("./model/index");
 const app = express();
-app.set("view engine", ejs);
+app.set("view engine", "ejs");
+
+require("./model/index");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(3000, (req, res) => {
-  console.log("hello from auth folder");
+  console.log("App started");
 });
 
 app.get("/", (req, res) => {
@@ -12,4 +18,25 @@ app.get("/", (req, res) => {
 
 app.get("/register", (req, res) => {
   res.render("register");
+});
+
+app.post("/register",async (req, res) => {
+  console.log(req.body);
+  
+    const {email, username, password} = req.body
+
+
+    //validation 
+    //types: here on api, form(frontend), in the model ex: allowNull: false
+    if(!email || !username || !password){
+      return res.send("please fill email, username, password")
+    }
+   await users.create({
+      email: email,
+      username: username,
+      password: password,
+    }
+    )
+    res.send('user creted successfully')
+  
 });

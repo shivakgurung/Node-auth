9cf4c033bff9168ac4a15d95188890c621a3ba80
Shiva Gurung <shivakgrg@gmail.com>
2023-09-28 22:50:56 +0545

    Implement bcrypt to hash password

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 app.set("view engine", "ejs");
 
 require("./model/index");
+const bcrypt = require("bcryptjs");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -20,23 +21,20 @@ app.get("/register", (req, res) => {
   res.render("register");
 });
 
-app.post("/register",async (req, res) => {
+app.post("/register", async (req, res) => {
   console.log(req.body);
-  
-    const {email, username, password} = req.body
 
+  const { email, username, password } = req.body;
 
-    //validation 
-    //types: here on api, form(frontend), in the model ex: allowNull: false
-    if(!email || !username || !password){
-      return res.send("please fill email, username, password")
-    }
-   await users.create({
-      email: email,
-      username: username,
-      password: password,
-    }
-    )
-    res.send('user creted successfully')
-  
+  //validation
+  //types: here on api, form(frontend), in the model ex: allowNull: false
+  if (!email || !username || !password) {
+    return res.send("please fill email, username, password");
+  }
+  await users.create({
+    email: email,
+    username: username,
+    password: bcrypt.hashSync(password, 8),
+  });
+  res.send("user creted successfully");
 });
